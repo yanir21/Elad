24ec02a1e283123d78e7ebd22e7a99e98bbfa810
jonathan <jonathanharmatz@gmail.com>
2021-07-26 20:17:37 +0300

    Updated dal

--- telegrambot/dal.py
+++ telegrambot/dal.py
@@ -1,12 +1,12 @@
-def get_database():
-    from pymongo import MongoClient
-    import pymongo
+import pymongo
+from bson.json_util import dumps
 
+def get_database():
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
     CONNECTION_STRING = "mongodb://elad:Qh3QYbnabuBUiANAlzFdl02Y2xUqVBZffZretLZDAUVRkA6IJhbYfkGcK6DUe1GOTqZD0AHSN6AcotifgHDB0Q==@elad.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@elad@"
 
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-    client = MongoClient(CONNECTION_STRING)
+    client = pymongo.MongoClient(CONNECTION_STRING)
 
     # Create the database for our example (we will use the same database throughout the tutorial
     return client['elad']
@@ -17,8 +17,13 @@ def get_collection():
     # Create a new collection
     collection_name = dbname["OptionTree"]
 
-    tree = collection_name.find()
-    return tree
-    
-if __name__ == "__main__":    
-    
+    collection = collection_name.find()
+    final = []
+
+    for item in collection:
+        final.append(item)
+
+    return (final)
+
+#if __name__ == "__main__":    
+#   print(get_collection())
