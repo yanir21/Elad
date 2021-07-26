e2c64f9a9454a3f910eb911e966baf545c9b8692
yuval balas <yuvalbalas10@gmail.com>
2021-07-26 20:18:08 +0300

    fix intents

--- format-intents.py
+++ format-intents.py
@@ -5,24 +5,6 @@ def format_nodes(nodes_from_db):
 def add_patterns_to_father_node(node):
     node_patterns = node.patterns
 
-def _finditem(obj, key):
-    if key in obj: return obj[key]
-    for k, v in obj.items():
-        if isinstance(v,dict):
-            item = _finditem(v, key)
-            if item is not None:
-                return item
-
-
-def tree_traverse(tree, key):
-    for k, v  in tree.items():
-        if k == key:
-            return v
-        elif isinstance(v, dict):
-            found = tree_traverse(v, key) 
-            if found is not None:  # check if recursive call found it
-                return found
-
 def get_node_by_id(node, id):
     if node["id"] == id:
         return node
